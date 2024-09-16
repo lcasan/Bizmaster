722c2ceae408935df5819c594213f2cd52eda9f3
lcasan <lcasan120100@gmail.com>
2024-09-16 01:33:41 -0400

    Added table formatting to output data using rich in show subcommands

--- src/cli/subcommands/show.py
+++ src/cli/subcommands/show.py
@@ -1,4 +1,6 @@
 import click
+from rich.table import Table
+from rich.console import Console
 from ..commands import show
 from sqlalchemy import select
 from ...database.connection import Session
@@ -7,16 +9,37 @@ from ...database.models import Client, Provider
 @show.command()
 @click.option('--select', default='all')
 def client(select):
+    # Create table:
+    table = Table(title = 'Clients')
+
+    table.add_column('Id', style = 'cyan')
+    table.add_column('Name', style = 'green')
+
     if select == 'all':
         clients = Client.read(sess = Session)
         for client in clients:
-            print(client[0])
+            table.add_row(str(client[0].id), str(client[0].name))
+
+    # Console
+    console = Console()
+    console.print(table)
 
 
 @show.command()
 @click.option('--select', default='all')
 def provider(select):
+    # Create table:
+    table = Table(title = 'Providers')
+
+    table.add_column('Id', style = 'cyan')
+    table.add_column('Name', style = 'magenta')
+
+    # Query
     if select == 'all':
         providers = Provider.read(sess = Session)
         for provider in providers:
-            print(provider[0])
+            table.add_row(str(provider[0].id), str(provider[0].name))
+    
+    # Console
+    console = Console()
+    console.print(table)
